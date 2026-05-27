60581b16a6dfc9b60f00b96f6fc89f6572d70fdf
iv221s29 <iv221s29@csc.sibsutis.ru>
2026-05-27 20:21:03 +0700

    done

--- homework/SellApp/SellApp/screens/P2PScreen/P2PDTO.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-struct P2PExchangeRateDTO { // raw data from api
-    let fromCurrency: String
-    let toCurrency: String
-    let rate: Double
-}
--- homework/SellApp/SellApp/services/AppConfig.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-enum AppConfig {
-    static let minOperationsPerDay: Int = 5
-    static let maxOperationsPerDay: Int = 15
-    static let tradingDaysCount: Int = 10
-    static let creditTopUpAmount: Double = 1000.0
-    static let initialBalance: Double = 1000.0
-}
